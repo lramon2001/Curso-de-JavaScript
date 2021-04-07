22b0ae89a46b4ff9a7391b2d7e67f768ceb49621
Lucas Ramon <lucasoliveira.zetta@ufla.br>
2021-04-07 14:20:13 -0300

    agora tá ok

--- Exercícios/Ex1/scriptEx1.js
+++ Exercícios/Ex1/scriptEx1.js
@@ -1,33 +1,39 @@
 function carregar(){
     var msg=window.document.getElementById('msg')
-    var img=window.document.getElementById('imagem')
     var dia = new Date();
+    var fotografia= window.document.getElementById('fotografia')
     var hora=dia.getHours();
     var minutos=dia.getMinutes();
     var saudacao=window.document.getElementById('saudacao')
     msg.innerHTML=`São ${hora} horas e ${minutos} minutos`
-  hora=4
+    var img=document.createElement('img')
+    img.setAttribute('id','foto')
+    
+    
 
 
     if(hora>=6 && hora<=12){
          saudacao.innerHTML="Você não pode mudar o seu passado mas pode estragar o seu futuro"
-         img.innerHTML='<img src="bomdia.png">'
+       
          document.body.style.background='#e2cd9f'
-         
+         img.setAttribute('src','bomdia.png')
     }
     else if(hora>12 && hora<=18){
         saudacao.innerHTML="Você é mais fraco do que pensa !!!"
-        img.innerHTML='<img src="boatarde.png">'
+       
         document.body.style.background='#b9846f'
+        img.setAttribute('src','boatarde.png')
     }
     else if(hora>18 && hora<=23){
         saudacao.innerHTML="Nenhum obstáculo é grande para quem desiste !!!"
-        img.scr='<img src="boanoite.png">'
+        img.setAttribute('src','boanoite.png')
         document.body.style.background='#2c2f33'
     }
     else{
         saudacao.innerHTML="O recomeço é a oportunidade de um novo fracasso !!!"
-        img.scr='<img src="boamadrugada.png">'
+        img.setAttribute('src','boamadrugada.png')
         document.body.style.background='#000000'
     }
+    fotografia.appendChild(img)
+
 }
