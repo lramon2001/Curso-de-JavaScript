4524adfcc3cd138faf8bf86b3f8916bb98fbdb35
Lucas Ramon <lucasoliveira.zetta@ufla.br>
2021-04-15 09:34:23 -0300

    modulo4

---
+++ Treinaweb/Modulos/Modulo4/Aulas/aula1.js
@@ -0,0 +1,11 @@
+//Tabela verdade do &&- e- and
+console.log(false && false)
+console.log(false && true)
+console.log(true && false)
+console.log(true && true)
+
+//Tabela verdade do ||- ou- or
+console.log(false || false)
+console.log(false || false)
+console.log(false || false)
+console.log(false || false)
---
+++ Treinaweb/Modulos/Modulo4/Aulas/aula2.js
@@ -0,0 +1,15 @@
+//Testando operadores 
+//soma
+console.log(4+3)
+//subtração
+console.log(4-3)
+//multiplicação
+console.log(4*3)
+//divisão
+console.log(8/2)
+//potenciação
+console.log(4**3)
+//radiciação
+console.log(4**(0.5))
+//Resto da divisão inteira
+console.log(5%2)
