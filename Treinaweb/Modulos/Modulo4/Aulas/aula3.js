849a0bc900cd6df6b79c4485566514529b609547
Lucas Ramon <lucasoliveira.zetta@ufla.br>
2021-04-15 10:49:18 -0300

    modulo4

---
+++ Treinaweb/Modulos/Modulo4/Aulas/aula3.js
@@ -0,0 +1,11 @@
+// Operadores de igualdade
+
+// == ou != --Compara o valor
+console.log(3=='3')
+console.log(3==2)
+console.log(3!='3')
+console.log(3!=2)
+//=== ou !== --Compara o valor e o tipo
+console.log(3==='3')
+console.log(3!==2)
+
