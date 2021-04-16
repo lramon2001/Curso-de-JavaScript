cecaee8dff5d0822530fbd249fb73ee7c656a7dc
Lucas Ramon <lucasoliveira.zetta@ufla.br>
2021-04-16 09:04:28 -0300

    modulo 5 ok

---
+++ Treinaweb/Modulos/Modulo5/Desafios/desafio2.js
@@ -0,0 +1,4 @@
+function main(massa, altura){
+    let imc= massa/(altura**2)
+       console.log(imc.toFixed(2));//toFixed define o número de casas decimais
+    }
---
+++ Treinaweb/Modulos/Modulo5/Desafios/desafio3.js
@@ -0,0 +1,8 @@
+function main(a, b){
+    let temp;
+    temp=a;
+    a=b;
+    b=temp;
+   console.log(`a = ${a}`);
+   console.log(`b = ${b}`);
+}
